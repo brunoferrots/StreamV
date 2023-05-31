dc8dec2cb7101431f47565f0f5b5e1c0209f8a7c
bruno <bruno@pop-os.localdomain>
2023-05-31 10:10:20 -0300

    Start project with spring initializr

---
+++ src/main/java/com/lychee/streamv/StreamVApplication.java
@@ -0,0 +1,13 @@
+package com.lychee.streamv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamVApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamVApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lychee/streamv/StreamVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lychee.streamv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamVApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
