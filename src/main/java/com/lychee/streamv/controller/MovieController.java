ea613412ef2da4652e6e4eed61ac6892947dd533
bruno <bruno@pop-os.localdomain>
2023-06-02 11:35:59 -0300

    Implementation functionality of create movie and start in the list movie

--- src/main/java/com/lychee/streamv/controller/MovieController.java
+++ src/main/java/com/lychee/streamv/controller/MovieController.java
@@ -1,15 +1,35 @@
 package com.lychee.streamv.controller;
 
+import com.lychee.streamv.domain.movie.Movie;
+import com.lychee.streamv.domain.movie.MovieDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
+    private List<Movie> movies = new ArrayList<>();
 
-    @GetMapping
+    @GetMapping("/form")
     public String renderPageForm() {
+        return "movies/form";
+    }
+    @GetMapping
+    public String renderPageList() {
+        return "movies/list";
+    }
+
+    @PostMapping
+    public String createMovie(MovieDTO movieDTO) {
+        var movie = new Movie(movieDTO);
+        movies.add(movie);
+        System.out.println(movies);
+
         return "movies/form";
     }
 }
